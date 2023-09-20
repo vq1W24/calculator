32a4e7c3ab916109d7adb7acd625e15797b197a0
vq1W24 <121136431+vq1W24@users.noreply.github.com>
2023-09-20 14:13:35 +0300

    Add files via upload

---
+++ work.cpp
@@ -0,0 +1,51 @@
+#include <iostream>  
+#include <cmath>  
+ 
+int main(){  
+    int a;  
+    int b;  
+    char operation;  
+    std::cout << "Введите первое число: ";  
+    std::cin >> a;  
+    std::cout << "Введите второе число: ";  
+    std::cin >> b;  
+    std::cout << "Выберите операцию: +,-,*,/,^,! ";  
+    std::cin >> operation;  
+   
+    switch (operation){  
+        case '-':  
+            std::cout << a - b;  
+            break;  
+        case '+':  
+            std::cout << a + b ;  
+            break;  
+        case '*':  
+            std::cout  << a * b ;  
+            break;  
+        case '/': 
+        if (a && b == 0)
+        std::cout << "Error" << std::endl;
+        else
+            std::cout  << a / b;  
+            break;  
+        case '^': { 
+            int result = 1; 
+            for (int i = 1; i <= b; ++i) { 
+                result *= a; 
+            } 
+            std::cout << result;
+            break;  
+        } 
+        case '!': { 
+            int factorial = 1;  
+            for (int i = 1; i <= a; i++) {  
+                factorial *= i;  
+            }  
+            std::cout << "Факториал " << a << ": " << factorial; 
+            break; 
+        } 
+        default:  
+            std::cout << "Ошибка";  
+    }  
+    return 0;  
+}
